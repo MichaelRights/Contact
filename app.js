f0711d88422d44c4c97cf32ada9b31a84e28a541
MichaelRight <melkumyan94@gmail.com>
2021-08-09 15:20:05 +0400

    build

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static("build"));
+app.use(express.static("client"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "index.html"));
 });
 
 app.listen(8080, "0.0.0.0", () => {
